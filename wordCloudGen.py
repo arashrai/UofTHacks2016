e7cfcfe1c911ff59dd92d8c2a7612d051b28a926
Arash Rai <arashrai17@gmail.com>
2016-01-24 03:47:10 -0500

    Updated Word Cloud

--- wordCloudGen.py
+++ wordCloudGen.py
@@ -1,23 +1,56 @@
+k = open('datanohtml.txt',"r", encoding = 'utf-8')
 
-f = open('count.txt', "r")
+lines = k.readlines()
 
-lines = f.readlines()
-string = ""
+L4 = []
 
-L1 = []
-
-for line in lines:
-    print(line)
-    k = line[:line.index(':')]
-    k = k.replace(' ','')
-    n = int(line[line.index(':')+1:])
-    L1.append([k,n])
+for x in lines:
+    L5 = x.split("$%^&")
+    for y in range(len(L5)):
+        k = L5[y].strip()
+        L5[y] = k
+    if len(L5) == 4:
+        L4.append(L5[3])
 
 
-for x in L1:
-    string += (x[0]+' ')*int(x[1]/10)
+parsedList = []
 
-with open("wordMap.txt", "a") as text_file:
-    print(string, file = text_file)
+for x in range(len(L4)):
+    a = L4[x].replace(',','')
+    b = a.replace('.','')
+    c = b.replace('-','')
+    d = c.replace('/','')
+    e = d.replace('(','')
+    f = e.replace(')','')
+    g = f.replace('?','')
+    h = g.replace('*','')
+    h = h.strip()
+    h = h.lower()
+    h = " " +h+" "
+    parsedList.append(h)
 
-    
+tools = []
+
+j = open('moretools2.txt',"r")
+
+lines = j.readlines()
+
+for x in lines:
+    tools.append([0,x[:-1]])
+
+for x in tools:
+    for y in parsedList:
+        string = " "+x[1]+" "
+        if string in y:
+            x[0] += 1
+
+tools = sorted(tools)
+tools.reverse()
+
+with open("count.txt","w") as text_file:
+    for x in tools:
+        print(x[1] + " ", x[0], file = text_file)
+
+with open("wordMap.txt","w") as text_file:
+    for x in tools:
+        print((x[1]+" ")*int(x[0]/10), file = text_file)
